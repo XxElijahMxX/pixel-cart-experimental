efb61f1d41988f0b473b4f1f3a290827858a9516
Mugen <melton.sheridan@gmail.com>
2023-01-21 16:42:28 -0500

    assigned props to heroBanner using data from Sanity

--- pixel-cart-experimental/components/HeroBanner.jsx
+++ pixel-cart-experimental/components/HeroBanner.jsx
@@ -1,21 +1,24 @@
 import Link from 'next/link'
 import React from 'react'
 
-const HeroBanner = () => {
+import { urlFor } from '../lib/client';
+
+const HeroBanner = ({ heroBanner }) => {
   return (
     <div className='hero-banner-container'>
         <div>
-            <p className='ps2-solo'>SMALL TEXT</p>
-            <h3>MID TEXT</h3>
-            <img src='' alt='playstation 2' className='hero-banner-image' />
+            <p className='ps2-solo'>{heroBanner.smallText}</p>
+            <h3>{heroBanner.midText}</h3>
+            <h1>{heroBanner.largeText1}</h1>
+            <img src={urlFor(heroBanner.image)} alt='playstation 2' className='hero-banner-image' />
 
             <div>
                 <Link href="/product/ID">
-                    <button type="button">BUTTON TEXT</button>
+                    <button type="button">{heroBanner.buttonText}</button>
                 </Link>
                 <div className='desc'>
                     <h5>Description</h5>
-                    <p>DESCRIPTION</p>
+                    <p>{heroBanner.desc}</p>
                 </div>
             </div>
         </div>
