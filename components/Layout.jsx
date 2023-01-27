5372887a2dd5a6a5323b359b90939e7a1a2cf773
Elijah <melton.sheridan@gmail.com>
2023-01-26 22:32:46 -0500

    changing file names to fix vercel deployment

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 
 import Navbar  from './Navbar'
-import Footer from './Footer'
+import Footer from './footer'
 
 const Layout = ({ children }) => {
   return (
